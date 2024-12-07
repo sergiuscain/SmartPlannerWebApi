5d0647564c4e94ebe16b2bcbb1550ba916ef418e
sergiuscain <sergiussmithe@gmail.com>
2024-12-07 16:33:26 +0300

    Создал первый контроллер и два тестовых метода для него

--- SmartPlannerWebApi/Controllers/NotesController.cs
+++ SmartPlannerWebApi/Controllers/NotesController.cs
@@ -2,8 +2,19 @@
 
 namespace SmartPlannerWebApi.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]   //Доступ к контроллеру осуществляется через https://localhost:7210/api/notes/
     public class NotesController : ControllerBase
     {
-
+        [HttpGet("test")]  //https://localhost:7210/api/notes/test
+        public string TestNotes()
+        {
+            return "Test Notes";
+        }
+        [HttpGet("test2")]  //https://localhost:7210/api/notes/test2
+        public string TestNotes2()
+        {
+            return "Nice Test^^";
+        }
     }
 }
