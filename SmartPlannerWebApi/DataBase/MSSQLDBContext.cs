1e020082f86acfe33b4081f70a68058d352fa860
sergiuscain <sergiussmithe@gmail.com>
2024-12-12 20:01:39 +0300

    Добавил Контекст базы данных

---
+++ SmartPlannerWebApi/DataBase/MSSQLDBContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SmartPlannerWebApi.Models;
+
+namespace SmartPlannerWebApi.DataBase
+{
+    public class MSSQLDBContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public MSSQLDBContext(DbContextOptions<MSSQLDBContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
